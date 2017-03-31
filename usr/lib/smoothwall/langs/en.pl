907f39df9b1d0ffa8881504bad66e3d9e53c3ff6
dataking <dataking@gmail.com>
2017-03-31 12:05:01 -0700

    Initial content commit for this repo

---
+++ usr/lib/smoothwall/langs/en.pl
@@ -0,0 +1,8 @@
+%basetr = (
+	%basetr,
+
+	'block icmp ping green'		=>	'Block ICMP ping on <span style="color:#00aa00;">GREEN</span>:',
+	'block icmp ping red'		=>	'Block ICMP ping on <span style="color:#cc0000;">RED</span>:',
+	'block icmp ping purple'	=>	'Block ICMP ping on <span style="color:#800080;">PURPLE</span>:',
+	'block ismp ping orange'	=>	'Block ICMP ping on <span style="color:#f87217;">ORANGE</span>:',
+);
